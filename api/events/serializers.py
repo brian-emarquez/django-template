aa01dc38ccc2dcdc37d9e5ae1ddd077be3f5555a
brian-emarquez <brian3marquez@gmail.com>
2024-05-30 14:09:40 -0500

    bandera compilado

---
+++ api/events/serializers.py
@@ -0,0 +1,8 @@
+# events/serializers.py
+from rest_framework import serializers
+from .models import Event
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['id', 'name', 'description', 'location', 'date']
--- api/events/views.py
+++ api/events/views.py
@@ -1,7 +1,19 @@
 # events/views.py
+from rest_framework import generics
+
 from django.shortcuts import render, redirect
 from .forms import EventForm
 from .models import Event
+from .serializers import EventSerializer
+
+class EventListCreate(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
 
 def create_event(request):
     if request.method == 'POST':
